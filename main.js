2e3cebd75282d8f4bfe37ba2ad48cc9c61f1b098
Sahithi5050 <98243648+Sahithi5050@users.noreply.github.com>
2023-04-01 19:40:34 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    recognition.start();
+} 
+
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+
+
+function speak(){
+
+    
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+    setTimeout(function(){
+        takeSnapshot();
+    
+    }, 5000);
+}
+function takeSnapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="imageSelfie" src="'+data_uri+'"/>';
+
+    })
+}
